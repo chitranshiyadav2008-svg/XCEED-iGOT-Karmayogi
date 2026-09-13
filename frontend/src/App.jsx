9bd440eceb22e7044691ebd913016e4a0caa3d40
Chitranshi Yadav <chitranshiyadav2008@gmail.com>
2026-09-13 08:36:30 +0530

    Add personalised learning path page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
 import Onboarding from "./pages/onboarding";
+import UpdatedSkillTwin from "./pages/updatedSkillTwin";
+import PersonalisedLearningPath from "./pages/personalisedLearningPath";
 
 function App() {
+  const path = window.location.pathname;
+
+  if (path === "/updated-skill-twin") {
+    return <UpdatedSkillTwin />;
+  }
+
+  if (path === "/learning-path") {
+    return <PersonalisedLearningPath />;
+  }
+
   return <Onboarding />;
 }
 
