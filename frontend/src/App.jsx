49ae3156b681ec1fe90699b30a8db441350aab59
ANNU SUHAG <annusuhag@MacBook-Air-von-ANNU.local>
2026-09-13 10:36:00 +0530

    Add Skill Gap Analysis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
-import Onboarding from "./pages/onboarding";
-import UpdatedSkillTwin from "./pages/updatedSkillTwin";
-import PersonalisedLearningPath from "./pages/personalisedLearningPath";
+import Onboarding from './pages/onboarding.jsx'
+import UpdatedSkillTwin from './pages/updatedSkillTwin.jsx'
+import PersonalisedLearningPath from './pages/personalisedLearningPath.jsx'
+import SkillGapAnalysis from './pages/skillGapAnalysis.jsx'
 
 function App() {
-  const path = window.location.pathname;
+  const path = window.location.pathname
 
-  if (path === "/updated-skill-twin") {
-    return <UpdatedSkillTwin />;
+  if (path === '/updated-skill-twin') {
+    return <UpdatedSkillTwin />
   }
 
-  if (path === "/learning-path") {
-    return <PersonalisedLearningPath />;
+  if (path === '/learning-path') {
+    return <PersonalisedLearningPath />
   }
 
-  return <Onboarding />;
+  if (path === '/skill-gap') {
+    return <SkillGapAnalysis />
+  }
+
+  return <Onboarding />
 }
 
-export default App;
+export default App
